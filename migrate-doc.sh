a8c4d1c97e7a9ae4b54e1f7753d5fcfc51aca308
Pepe <pepe@yuilop.com>
2014-01-09 10:55:06 +0100

    Script for create a github page with the project doc

---
+++ migrate-doc.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+echo "Migrates an existing doc folder into a gh-pages branch, and links back as submodule"
+    
+current_branch=`git branch | grep "^*" | sed -e "s/* //"`
+repo=`git config --list | grep "^remote.origin.url" | sed -e "s/remote.origin.url=//"`
+website_folder='doc'
+tmp_folder="/tmp/gh-pages-website"
+echo "Moving $website_folder folder to branch gh-pages."
+echo "Working in $current_branch$ branch of $repo:"
+
+rm -rf $tmp_folder
+mv $website_folder $tmp_folder
+
+echo "temporarily removing $website_folder whilst moving to gh-pages branch"
+git commit -a -m "temporarily removing $website_folder whilst moving to gh-pages branch"
+git symbolic-ref HEAD refs/heads/gh-pages
+rm .git/index
+git clean -fdx
+cp -R $tmp_folder/* .
+git add .
+echo "Import original $website_folder folder from $current_branch branch"
+git commit -a -m "Import original $website_folder folder from $current_branch branch"
+git push origin gh-pages
+git checkout $current_branch
+git submodule add -b gh-pages $repo $website_folder
+echo "migrated $website_folder -> gh-pages branch, and replaced with submodule link"
+git commit -a -m "migrated $website_folder -> gh-pages branch, and replaced with submodule link"
+git push
